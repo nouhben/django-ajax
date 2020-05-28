9c26d585dbd356855ca21231d5d079506706691c
nouh ben kadi <benkadi.nouh@icloud.com>
2020-05-28 10:59:05 +0100

    ✨ finnishing vanilla js ajax calls need to check

--- app_01/static/app_01/js/main.js
+++ app_01/static/app_01/js/main.js
@@ -1,31 +1,53 @@
-$(document).ready(function () {
-    $('#get-btn').click(function (e) {
-        $.ajax({
-            url: '', //where to send the request to the django webserver
-            type: 'GET', //i want to read somthng from server
-            data: {
-                //data that i want to send to the server
-                //the 'this' refers to "$('#get-btn)"
-                btn_text: $(this).text() //use this key on the django view
-            },
-            success: function (response) {
-                //response is the response i get from the server == django view handler 
-                //me: request  ----> view : server
-                //me: response <---- view
-                $('#get-btn').text(response.seconds);
-                let x = 0;
-                for (var i = 0; i < 10000000000; i++) {
-                    //do nothing
-                    x *= i / 2 + 1;
-                }
-                $('#seconds').append(
-                    '<li class="card p-3 mt-3">t = ' + response.seconds + '</li>'
-                );
-            },
-        })
-    });
-});
+// $(document).ready(function () {
+//     $('#get-btn').click(function (e) {
+//         $.ajax({
+//             url: '', //where to send the request to the django webserver
+//             type: 'GET', //i want to read somthng from server
+//             data: {
+//                 //data that i want to send to the server
+//                 //the 'this' refers to "$('#get-btn)"
+//                 btn_text: $(this).text() //use this key on the django view
+//             },
+//             success: function (response) {
+//                 //response is the response i get from the server == django view handler 
+//                 //me: request  ----> view : server
+//                 //me: response <---- view
+//                 $('#get-btn').text(response.seconds);
+//                 let x = 0;
+//                 for (var i = 0; i < 10000000000; i++) {
+//                     //do nothing
+//                     x *= i / 2 + 1;
+//                 }
+//                 $('#seconds').append(
+//                     '<li class="card p-3 mt-3">t = ' + response.seconds + '</li>'
+//                 );
+//             },
+//         })
+//     });
+// });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const getBtn = document.getElementById('get-btn');
+    getBtn.addEventListener('click', function (e) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', '', true);
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                const newItem = document.createElement('li');
+                ['card', 'p-3', 'mt-3'].forEach(c => {
+                    newItem.classList.add(c);
+                });
+                console.log(JSON.parse(xhr.responseText))
+                newItem.appendChild(document.createTextNode(JSON.parse(xhr.responseText)));
+                document.getElementById('seconds').appendChild(newItem);
+            }
+        };
+        xhr.onerror = function () {
+            console.log('hello world: error');
+        }
+        xhr.send();
+    });
+})
 
 
 //ajax form
